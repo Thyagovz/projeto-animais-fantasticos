efb59b1844f195c0a9f46dbc8b8aa9a64c361351
Thyago Euclides <flooddk@gmail.com>
2025-08-30 20:38:31 -0400

    feat: refactor navigation and animation scripts; implement data attributes for improved functionality

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,22 @@
+export default function initAnimationScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) {
+          section.classList.add("active");
+        } else {
+          section.classList.remove("active");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+
+initAccordion();
+initScrollSuave();
+initAnimationScroll();
+initTabNav();
