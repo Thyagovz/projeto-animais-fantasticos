3a1b552f4b7a856b8060d7d3ab0996382ca012fc
Thyago Euclides <flooddk@gmail.com>
2025-08-31 16:35:46 -0400

    feat: add modal functionality with toggle and close features; update HTML and JavaScript for modal integration

---
+++ js/modules/modal.js
@@ -0,0 +1,22 @@
+export default function initModal() {
+  const showButton = document.querySelector('[data-modal="show"]');
+  const closeButton = document.querySelector('[data-modal="close"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  if (showButton && closeButton && containerModal) {
+    function toggleModal(e) {
+      e.preventDefault();
+      containerModal.classList.toggle("active");
+    }
+
+    function clickOutModal(e) {
+      if (e.target === this) {
+        toggleModal(e);
+      }
+    }
+
+    showButton.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", clickOutModal);
+  }
+}
