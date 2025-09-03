0798a6c681d8b3d645a55f4257c0f40b77f551e9
Thyago Euclides <flooddk@gmail.com>
2025-09-03 16:11:47 -0400

    feat: refactor animal numbers section; remove hardcoded values and implement dynamic fetching from JSON

---
+++ js/modules/fetch-animals.js
@@ -0,0 +1,25 @@
+import initAnimateNumbers from "./animate-numbers.js";
+
+export default function initFetchAnimals() {
+  async function FetchAnimals(url) {
+    const animalsResponse = await fetch(url);
+    const animalsJSON = await animalsResponse.json();
+    const numbersGrid = document.querySelector(".numbers-grid");
+
+    animalsJSON.forEach((animal) => {
+      const divAnimal = createAnimal(animal);
+      numbersGrid.appendChild(divAnimal);
+    });
+    initAnimateNumbers();
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("number-animal");
+
+    div.innerHTML = `<h3> ${animal.specie}</h3><span data-number>${animal.total}</span>`;
+    return div;
+  }
+
+  FetchAnimals("./animalsapi.json");
+}
